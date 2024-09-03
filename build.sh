173f92422557180113eb78bfe04cc3cba587c091
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2024-09-03 17:36:33 +0200

    Tiny bash script refactor

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-rm -rf pkg
-rm -rf pkg-node
-rm -rf pkg-web
-wasm-pack build -t nodejs
+rm -rf pkg pkg-node pkg-web
+wasm-pack build --target nodejs
 mv pkg pkg-node
 wasm-pack build
 mv pkg pkg-web
